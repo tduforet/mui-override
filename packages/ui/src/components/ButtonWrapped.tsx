508ae2a02a3c7cc6e9fbf4a510a7c9c9446489a4
tduforet <thomas.duforet@icloud.com>
2024-07-13 10:33:51 +0200

    add display name on ButtonWrapped

--- packages/ui/src/components/ButtonWrapped.tsx
+++ packages/ui/src/components/ButtonWrapped.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import {Button as MuiButton, type ButtonProps as MuiButtonProps,} from "@mui/material";
+import {Button as MuiButton, type ButtonProps as MuiButtonProps} from "@mui/material";
 
 
 export const ButtonWrapped = ({children, ...rest}: MuiButtonProps) => (
   <MuiButton {...rest}>{children}</MuiButton>
 );
+
+ButtonWrapped.displayName = 'ButtonWrapped'
